2cd9cb946805fcb2660f4e66c5ba6f7b76fb1e6e
Marty-Massucci <98054244+Marty-Massucci@users.noreply.github.com>
2022-02-15 18:11:06 -0500

    Add files via upload

---
+++ HW3/HW3/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace HW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BinaryPictureBox = new System.Windows.Forms.PictureBox();
+            this.column1 = new System.Windows.Forms.Label();
+            this.column2 = new System.Windows.Forms.Label();
+            this.column3 = new System.Windows.Forms.Label();
+            this.column4 = new System.Windows.Forms.Label();
+            this.column5 = new System.Windows.Forms.Label();
+            this.EmguPictureBox = new System.Windows.Forms.PictureBox();
+            this.ThresholdTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.BinaryPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EmguPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ThresholdTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BinaryPictureBox
+            // 
+            this.BinaryPictureBox.Location = new System.Drawing.Point(401, 12);
+            this.BinaryPictureBox.Name = "BinaryPictureBox";
+            this.BinaryPictureBox.Size = new System.Drawing.Size(369, 336);
+            this.BinaryPictureBox.TabIndex = 0;
+            this.BinaryPictureBox.TabStop = false;
+
+            // 
+            // column1
+            // 
+            this.column1.AutoSize = true;
+            this.column1.Location = new System.Drawing.Point(398, 351);
+            this.column1.Name = "column1";
+            this.column1.Size = new System.Drawing.Size(35, 13);
+            this.column1.TabIndex = 1;
+            this.column1.Text = "label1";
+            // 
+            // column2
+            // 
+            this.column2.AutoSize = true;
+            this.column2.Location = new System.Drawing.Point(481, 351);
+            this.column2.Name = "column2";
+            this.column2.Size = new System.Drawing.Size(35, 13);
+            this.column2.TabIndex = 2;
+            this.column2.Text = "label2";
+            // 
+            // column3
+            // 
+            this.column3.AutoSize = true;
+            this.column3.Location = new System.Drawing.Point(571, 351);
+            this.column3.Name = "column3";
+            this.column3.Size = new System.Drawing.Size(35, 13);
+            this.column3.TabIndex = 3;
+            this.column3.Text = "label3";
+            // 
+            // column4
+            // 
+            this.column4.AutoSize = true;
+            this.column4.Location = new System.Drawing.Point(670, 351);
+            this.column4.Name = "column4";
+            this.column4.Size = new System.Drawing.Size(35, 13);
+            this.column4.TabIndex = 4;
+            this.column4.Text = "label4";
+            // 
+            // column5
+            // 
+            this.column5.AutoSize = true;
+            this.column5.Location = new System.Drawing.Point(735, 351);
+            this.column5.Name = "column5";
+            this.column5.Size = new System.Drawing.Size(35, 13);
+            this.column5.TabIndex = 5;
+            this.column5.Text = "label5";
+            // 
+            // EmguPictureBox
+            // 
+            this.EmguPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.EmguPictureBox.Name = "EmguPictureBox";
+            this.EmguPictureBox.Size = new System.Drawing.Size(352, 336);
+            this.EmguPictureBox.TabIndex = 6;
+            this.EmguPictureBox.TabStop = false;
+            // 
+            // ThresholdTrackBar
+            // 
+            this.ThresholdTrackBar.Location = new System.Drawing.Point(12, 393);
+            this.ThresholdTrackBar.Maximum = 255;
+            this.ThresholdTrackBar.Name = "ThresholdTrackBar";
+            this.ThresholdTrackBar.Size = new System.Drawing.Size(776, 45);
+            this.ThresholdTrackBar.TabIndex = 7;
+            this.ThresholdTrackBar.Scroll += new System.EventHandler(this.ThresholdTrackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ThresholdTrackBar);
+            this.Controls.Add(this.EmguPictureBox);
+            this.Controls.Add(this.column5);
+            this.Controls.Add(this.column4);
+            this.Controls.Add(this.column3);
+            this.Controls.Add(this.column2);
+            this.Controls.Add(this.column1);
+            this.Controls.Add(this.BinaryPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.BinaryPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EmguPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ThresholdTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BinaryPictureBox;
+        private System.Windows.Forms.Label column1;
+        private System.Windows.Forms.Label column2;
+        private System.Windows.Forms.Label column3;
+        private System.Windows.Forms.Label column4;
+        private System.Windows.Forms.Label column5;
+        private System.Windows.Forms.PictureBox EmguPictureBox;
+        private System.Windows.Forms.TrackBar ThresholdTrackBar;
+    }
+}
+
---
+++ HW3/HW3/Form1.cs
@@ -0,0 +1,176 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW3
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        private int _threshold = 150;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            ThresholdTrackBar.Value = _threshold;
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+
+                int newHeight = (frame.Size.Height * EmguPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(EmguPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+
+                EmguPictureBox.Image = frame.Bitmap;
+
+                Mat grayFrame = new Mat();
+                Mat binaryFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(grayFrame, binaryFrame, _threshold, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+
+                BinaryPictureBox.Image = binaryFrame.Bitmap;
+                int sliceWidth = frame.Width / 5; 
+
+                /*
+                Image<Gray, byte> img = frame.ToImage<Gray, byte>();
+                img.ROI = new Rectangle(0, 0, frame.Width, frame.Height);
+                int whitePixelsFirst = img.CountNonzero()[0];
+                img.ROI = Rectangle.Empty;
+                Invoke(new Action(() =>
+                {
+                    column1.Text = $"{whitePixelsFirst}";
+                }));
+                */
+
+
+                //first fifth
+                int whitePixelsfirst = 0;
+                Image<Gray, byte> img2 = binaryFrame.ToImage<Gray, byte>();
+                for (int x = 0; x < sliceWidth; x++)
+                {
+                    for (int y = 0; y < binaryFrame.Height; y++)
+                    {
+                        if (img2.Data[y, x, 0] == 255)
+                        {
+                            whitePixelsfirst++;
+                        }
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    column1.Text = $"{whitePixelsfirst}";
+                }));
+
+                //second fifth
+                int whitePixelssecond = 0;
+                img2 = binaryFrame.ToImage<Gray, byte>();
+                for (int x = sliceWidth; x < 2 * sliceWidth; x++)
+                {
+                    for (int y = 0; y < binaryFrame.Height; y++)
+                    {
+                        if (img2.Data[y, x, 0] == 255)
+                        {
+                            whitePixelssecond++;
+                        }
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    column2.Text = $"{whitePixelssecond}";
+                }));
+
+                //third fifth
+                int whitePixelsthird = 0;
+                img2 = binaryFrame.ToImage<Gray, byte>();
+                for (int x = 2 * sliceWidth; x < 3 * sliceWidth; x++)
+                {
+                    for (int y = 0; y < binaryFrame.Height; y++)
+                    {
+                        if (img2.Data[y, x, 0] == 255)
+                        {
+                            whitePixelsthird++;
+                        }
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    column3.Text = $"{whitePixelsthird}";
+                }));
+
+                //fourth fifth
+                int whitePixelsfourth = 0;
+                img2 = binaryFrame.ToImage<Gray, byte>();
+                for (int x = 3 * sliceWidth; x < 4 * sliceWidth; x++)
+                {
+                    for (int y = 0; y < binaryFrame.Height; y++)
+                    {
+                        if (img2.Data[y, x, 0] == 255)
+                        {
+                            whitePixelsfourth++;
+                        }
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    column4.Text = $"{whitePixelsfourth}";
+                }));
+
+                //fifth fifth
+                int whitePixelsfifth = 0;
+                img2 = binaryFrame.ToImage<Gray, byte>();
+                for (int x = 4 * sliceWidth; x < binaryFrame.Width; x++)
+                {
+                    for (int y = 0; y < binaryFrame.Height; y++)
+                    {
+                        if (img2.Data[y, x, 0] == 255)
+                        {
+                            whitePixelsfifth++;
+                        }
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    column5.Text = $"{whitePixelsfifth}";
+                }));
+
+
+            }
+        }
+
+        
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+        private void ThresholdTrackBar_Scroll(object sender, EventArgs e)
+        {
+            _threshold = ThresholdTrackBar.Value;
+        }
+
+
+    }
+}
